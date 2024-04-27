a3a45ae71cdc2ec91fd4678f6e69d986903eb7ec
Kidult1002 <166999827+Kidult1002@users.noreply.github.com>
2024-04-27 18:27:08 +0800

    Update tulbur.js

--- tulbur.js
+++ tulbur.js
@@ -3,15 +3,7 @@ const golomt_bank = document.getElementById("golomt_bank");
 const mySubmit = document.getElementById("mySubmit");
 
 mySubmit.onclick = function() {
-    let x
-    if (khaan.checked) {
-       x = khaan.getAttribute('data-href');
-    } else if (golomt_bank.checked) {
-        x = golomt_bank.getAttribute('data-href');
-    }
-    if (x) {
-        window.location.href = x;
-    } else {
+    
         alert("Банкаа сонгоно уу?");
     }
 }
